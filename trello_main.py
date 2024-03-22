52173256ff835e021c3a2b0da2370fa685437df6
Grigory Petrov <grigoryvp@gmail.com>
2024-03-22 19:58:49 +0100

    caprover compatibility

--- trello_main.py
+++ trello_main.py
@@ -6,8 +6,6 @@ from trello import TrelloClient
 from dotenv import load_dotenv
 
 
-load_dotenv()
-
 def patched_fetch_json(self,
                        uri_path,
                        http_method='GET',
@@ -58,33 +56,43 @@ def patched_fetch_json(self,
 
     return response.json()
 
-TrelloClient.fetch_json = patched_fetch_json
+def main():
+
+    load_dotenv()
+
+    TrelloClient.fetch_json = patched_fetch_json
+    client = TrelloClient(
+        api_key=os.environ['TRELLO_API_KEY'],
+        api_secret=os.environ['TRELLO_API_SECRET']
+    )
+
+    print("getting organizations list...", flush=True)
+    org_list = client.list_organizations()
+    family_boards = []
+    for organization in org_list:
+        if "family" in organization.name.lower():
+            family_boards = organization.all_boards()
+    print(f"loaded {len(family_boards)} boards", flush=True)
 
 
-client = TrelloClient(
-    api_key=os.environ['TRELLO_API_KEY'],
-    api_secret=os.environ['TRELLO_API_SECRET']
-)
+    def move_all_tasks_in_todo(board):
+        board_lists = board.list_lists()
 
-org_list = client.list_organizations()
-for organization in org_list:
-    if "family" in organization.name.lower():
-        family_boards = organization.all_boards()
+        target_list = []
+        for list_ in board_lists:
+            spaceless_name = list_.name.lower().replace(" ", "")
+            if "todo" in spaceless_name:
+                target_list = list_
 
-
-def move_all_tasks_in_todo(board):
-    board_lists = board.list_lists()
-
-    for list_ in board_lists:
-        spaceless_name = list_.name.lower().replace(" ", "")
-        if "todo" in spaceless_name:
-            target_list = list_
-
-    for list_ in board_lists:
-        if list_ is not target_list:
-            list_.move_all_cards(target_list)
+        for list_ in board_lists:
+            if list_ is not target_list:
+                list_.move_all_cards(target_list)
 
 
-for board in family_boards:
-    if "tasks" in board.name.lower():
-        move_all_tasks_in_todo(board)
+    for board in family_boards:
+        if "tasks" in board.name.lower():
+            move_all_tasks_in_todo(board)
+
+
+if __name__ == "__main__":
+    main()
